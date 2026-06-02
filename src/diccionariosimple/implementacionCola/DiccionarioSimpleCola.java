3d6c9bde01211f048c5c10aca69d07de49b9f5ce
danosqui <danosqui20@gmail.com>
2026-06-02 20:49:03 -0300

    ej 5

---
+++ src/diccionariosimple/implementacionCola/DiccionarioSimpleCola.java
@@ -0,0 +1,146 @@
+package diccionariosimple.implementacionCola;
+
+import tda.ConjuntoTDA;
+import tda.DiccionarioSimpleTDA;
+import tda.ColaTDA;
+import imple.Cola;
+import imple.Conjunto;
+
+public class DiccionarioSimpleCola implements DiccionarioSimpleTDA {
+	
+	private ColaTDA datos;
+	private ColaTDA claves;
+
+	@Override
+	public void inicializarDiccionario() {
+		datos = new Cola();
+		datos.inicializarCola();
+		claves = new Cola();
+		claves.inicializarCola();
+	}
+
+	@Override
+	public void agregar(int clave, int valor) {
+		ColaTDA auxD = new Cola();
+		ColaTDA auxC = new Cola();
+		auxD.inicializarCola();
+		auxC.inicializarCola();
+		while (!datos.colaVacia() && claves.primero()!=clave) {
+			auxD.acolar(datos.primero());
+			auxC.acolar(claves.primero());
+			datos.desacolar();
+			claves.desacolar();
+		}
+		if (!datos.colaVacia()) {
+			
+			claves.desacolar();
+			datos.desacolar();
+		}
+		
+		datos.acolar(valor);
+		claves.acolar(clave);
+		System.out.println(datos.primero());
+		
+		while(!auxD.colaVacia()) { 
+			datos.acolar(auxD.primero());
+			claves.acolar(auxC.primero());
+			auxD.desacolar();
+			auxC.desacolar();
+		}
+	}
+
+	@Override
+	public ConjuntoTDA claves() {
+		ColaTDA aux = copiarCola(claves);
+		ConjuntoTDA claves2 = new Conjunto();
+		
+		
+		claves2.inicializarConjunto();
+		
+		while (!aux.colaVacia()) {
+			claves2.agregar(aux.primero());
+			aux.desacolar();
+		}
+		
+		
+		return claves2;
+	}
+
+	@Override
+	public void eliminar(int n) {
+		ColaTDA auxD = new Cola();
+		ColaTDA auxC = new Cola();
+		auxD.inicializarCola();
+		auxC.inicializarCola();
+		
+		while (!datos.colaVacia() && claves.primero()!=n) {
+			auxD.acolar(datos.primero());
+			auxC.acolar(claves.primero());
+			datos.desacolar();
+			claves.desacolar();
+		}
+		if (claves.primero()==n) {
+			claves.desacolar();
+			datos.desacolar();
+		}
+		
+		while(!auxD.colaVacia()) { //se desordena pero no importa porque es un diccio
+			datos.acolar(auxD.primero());
+			claves.acolar(auxC.primero());
+			auxD.desacolar();
+			auxC.desacolar();
+		}
+
+	}
+
+
+	@Override
+	public int recuperar(int n) {
+		ColaTDA auxD = new Cola();
+		ColaTDA auxC = new Cola();
+		auxD.inicializarCola();
+		auxC.inicializarCola();
+		
+		
+		while (!datos.colaVacia() && claves.primero()!=n) {
+			auxD.acolar(datos.primero());
+			auxC.acolar(claves.primero());
+			datos.desacolar();
+			claves.desacolar();
+		}
+		int ret;
+		if (!datos.colaVacia()) {
+			ret = datos.primero();
+		}
+		else {
+			throw new RuntimeException();
+		}
+		while(!auxD.colaVacia()) {
+			datos.acolar(auxD.primero());
+			claves.acolar(auxC.primero());
+			auxD.desacolar();
+			auxC.desacolar();
+		}
+		
+		return ret;
+	}
+	
+	private ColaTDA copiarCola(ColaTDA original) {
+		ColaTDA copia = new Cola();
+		ColaTDA aux = new Cola();
+		copia.inicializarCola();
+		aux.inicializarCola();
+		while (!original.colaVacia()) {
+			copia.acolar(original.primero());
+			aux.acolar(original.primero());
+			original.desacolar();
+		}
+		
+		while(!aux.colaVacia()) {
+			original.acolar(aux.primero());
+			aux.desacolar();
+		}
+		return copia;
+	}
+
+}
---
+++ src/diccionariosimple/uso/Uso.java
@@ -0,0 +1,66 @@
+package diccionariosimple.uso;
+
+import diccionariosimple.implementacionCola.DiccionarioSimpleCola;
+import imple.Conjunto;
+import tda.ConjuntoTDA;
+import tda.DiccionarioSimpleTDA;
+
+public class Uso {
+
+    public static void main(String[] args) {
+    	 
+        DiccionarioSimpleTDA dic = new DiccionarioSimpleCola();
+ 
+       
+        dic.inicializarDiccionario();
+ 
+    
+        dic.agregar(10, 100);
+        dic.agregar(20, 200);
+        dic.agregar(30, 300);
+        dic.agregar(40, 400);
+ 
+        System.out.println(dic.recuperar(10));
+        System.out.println(dic.recuperar(20));
+        System.out.println(dic.recuperar(30)); 
+        System.out.println(dic.recuperar(40));
+        imprimirClaves(dic);
+        dic.eliminar(20);
+        imprimirClaves(dic);
+ 
+        dic.agregar(10, 999);
+        System.out.println(dic.recuperar(10));
+        imprimirClaves(dic);
+ 
+        ConjuntoTDA clavesRestantes = dic.claves();
+        while (!clavesRestantes.conjuntoVacio()) {
+            int c = clavesRestantes.elegir();
+            dic.eliminar(c);
+            clavesRestantes.sacar(c);
+        }
+        imprimirClaves(dic);
+ 
+    }
+ 
+    static void imprimirClaves(DiccionarioSimpleTDA dic) {
+        ConjuntoTDA original = dic.claves();
+        
+        if (original.conjuntoVacio()) {
+            System.out.println("vacio");
+            return;
+        }
+ 
+        ConjuntoTDA aux = new Conjunto();
+        aux.inicializarConjunto();
+ 
+        while (!original.conjuntoVacio()) {
+            int clave = original.elegir();
+            System.out.println(clave + " val " + dic.recuperar(clave));
+            aux.agregar(clave);
+            original.sacar(clave);
+        }
+        System.out.println();
+    }
+
+
+}
